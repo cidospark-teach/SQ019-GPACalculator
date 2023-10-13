a9624bf779d8b7a3b892e80a8b739ca4ff71625f
Francis Ibe <49306271+Cidospark@users.noreply.github.com>
2023-10-13 15:44:59 +0100

    first commit

---
+++ GPACalculator/GPACalculator/GPACalculator/Program.cs
@@ -0,0 +1,121 @@
+﻿using GPACalculator.Logic.Commons;
+using GPACalculator.Logic.Core;
+using GPACalculator.Logic.Models;
+using GPACalculator.UI.ViewModel;
+using System;
+using System.Collections.Generic;
+
+namespace GPACalculator.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // user input
+            var inputs = GetInputs();
+
+            // map inputs to course records
+            var records = new List<CourseRecord>();
+            foreach(var record in inputs.ScoreInputs)
+            {
+                records.Add(new CourseRecord { CourseName = record.CourseName, CourseUnit = record.CourseUnit, Score = record.Score });
+            }
+
+            var cal = new Calculator();
+            try
+            {
+                // grade student
+                var result = cal.GetResult(records);
+
+                // map result to report
+                var report = new Report
+                {
+                    StudentName = inputs.StudentName,
+                    Results = new List<Result> { result }
+                };
+
+                // print report
+                DisplayReport(report);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Caught Error: "+ e.Message);
+            }         
+
+
+        }
+
+
+
+        // Display report
+        public static void DisplayReport(Report report)
+        {
+
+            if(report == null)
+                Console.WriteLine("Report is enpty!");
+
+            int widthOfTable = 85;
+            Console.Clear();
+
+            Utilities.PrintLine(widthOfTable);
+            Utilities.PrintRow(widthOfTable, $"RESULT FOR {report.StudentName.ToUpper()}");
+            Utilities.PrintLine(widthOfTable);
+
+            Utilities.PrintRow(widthOfTable, "COURSE NAME", "COURSE UNIT", "SCORE", "GRADE", "GRADE POINT", "QUALITY POINT");
+            Utilities.PrintLine(widthOfTable);
+            
+            foreach(var RP in report.Results[0].CourseRecords)
+            {
+                Utilities.PrintRow(widthOfTable, RP.CourseName, RP.CourseUnit.ToString(), RP.Score.ToString(), RP.Grade.ToString(), RP.GradeUnit.ToString(), RP.QualityPoint.ToString());
+            }
+            
+            Utilities.PrintLine(widthOfTable);
+
+            Console.WriteLine($"Your GPA is: "+ report.Results[0].GPA);
+
+            Console.WriteLine($"\nGPA is calculated using the formula: ");
+            Console.WriteLine("GPA = (total QP) / (total course units)");
+
+            Console.ReadLine();
+        }
+
+
+
+        // get inputs
+        public static InputForm GetInputs()
+        {
+            var input = new InputForm();
+
+            try
+            {
+                Console.Write("\nEnter your name: ");
+                input.StudentName = Console.ReadLine();
+
+
+                Console.Write("\nEnter number of courses: ");
+                var numberOfCourses = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < numberOfCourses; i++)
+                {
+                    Console.Write($"\n\n({i+1}) Enter course name eg(Maths-101): ");
+                    var courseName = Console.ReadLine();
+
+                    Console.Write("\nEnter course unit: ");
+                    var courseUnit = int.Parse(Console.ReadLine());
+
+                    Console.Write("\nEnter score: ");
+                    var score = int.Parse(Console.ReadLine());
+
+                    input.ScoreInputs.Add(new ScoreInput { CourseName = courseName, CourseUnit = courseUnit, Score = score });
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n\nCaught Error: " + e.Message);
+            }
+
+            return input;
+        }
+    }
+}
---
+++ GPACalculator/GPACalculator/GPACalculatorLogic/Core/Calculator.cs
@@ -0,0 +1,109 @@
+﻿using GPACalculator.Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPACalculator.Logic.Core
+{
+    public class Calculator
+    {
+
+
+        // calculate quality Point
+        public List<CourseRecord> CalculateQualityPoint(List<CourseRecord> records)
+        {
+            if (records == null)
+                throw new Exception("Null entry");
+
+            foreach (var record in records)
+            {
+                record.QualityPoint = record.CourseUnit * record.GradeUnit;
+            }
+            return records;
+        }
+
+
+        // calucluate GPA
+        public string CalculateGPA(List<CourseRecord> records)
+        {
+            if (records == null)
+                throw new Exception("Null entry");
+
+            double totalQualityPoint = 0;
+            double totalCourseUnit = 0;
+
+            foreach(var record in records)
+            {
+                totalQualityPoint += record.QualityPoint;
+                totalCourseUnit += record.CourseUnit;
+            }
+
+            var gpa = totalQualityPoint / totalCourseUnit;
+            // decimal.Round(yourValue, 2, MidpointRounding.AwayFromZero); if i want to return the value in decimal format
+            //return gpa.ToString("0.##");    with this formatting I can set to any decimal place. Just depends on the number of # I add after the dot      
+            return gpa.ToString("F");
+        }
+
+
+
+        // grade score
+        public List<CourseRecord> GetGrade(List<CourseRecord> records)
+        {
+            if (records == null)
+                throw new Exception("Null entry");
+
+            var gradeSystem = GradeSystem.Grades;
+            var found = false;
+
+            foreach(var record in records)
+            {
+                for(int i = 0; i < gradeSystem.Count; i++)
+                {
+                    if (record.Score >= gradeSystem[i].MinScore && record.Score <= gradeSystem[i].MaxScore)
+                    {
+                        record.Grade = gradeSystem[i].Grade;
+                        record.GradeUnit = gradeSystem[i].GradePoint;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    throw new Exception("No matching grade found for "+ record.Score);
+                }
+                
+            }
+
+            return records;
+            
+        }
+
+
+
+        // grade student
+        public Result GetResult(List<CourseRecord> records)
+        {
+            if (records == null)
+                throw new Exception("Null entry");
+
+            // Grade score
+            var gradedResult = GetGrade(records);
+
+            // calculate Quality Point
+            var calculatedQP = CalculateQualityPoint(gradedResult);
+
+            // calculate GPA
+            var calculatedGPA = CalculateGPA(calculatedQP);
+
+            var rs = new Result
+            {
+                CourseRecords = calculatedQP,
+                GPA = calculatedGPA
+            }; 
+
+            return rs;
+
+        }
+    }
+}
